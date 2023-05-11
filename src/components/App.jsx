57c9dce440612fc4aab984e4a530c25a2c9e832c
Serhii Lytvynenko <mr.litvinenko@gmail.com>
2023-05-11 17:34:57 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,102 +1,81 @@
-import React, { Component } from 'react';
+import { useState } from 'react';
 import Section from './Section';
 import FeedbackOptions from './FeedbackOptions';
 import Statistics from './Statistics';
 
 import css from './App.module.css';
 
-// ============== Class area ===================
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+  const handlerGoodBtn = () => {
+    setGood(prevState => prevState + 1);
   };
 
-  handleFeedback = type => {
-    this.setState(prevState => ({
-      [type]: prevState[type] + 1,
-    }));
+  const handlerNeutralBtn = () => {
+    setNeutral(prevState => prevState + 1);
   };
 
-  handlerGoodBtn = () => {
-    this.handleFeedback('good');
+  const handlerBadBtn = () => {
+    setBad(prevState => prevState + 1);
   };
 
-  handlerNeutralBtn = () => {
-    this.handleFeedback('neutral');
+  const onLeaveFeedback = {
+    handlerGoodBtn: handlerGoodBtn,
+    handlerNeutralBtn: handlerNeutralBtn,
+    handlerBadBtn: handlerBadBtn,
   };
 
-  handlerBadBtn = () => {
-    this.handleFeedback('bad');
+  const state = {
+    good: good,
+    neutral: neutral,
+    bad: bad,
   };
 
-  onLeaveFeedback = {
-    handlerGoodBtn: this.handlerGoodBtn,
-    handlerNeutralBtn: this.handlerNeutralBtn,
-    handlerBadBtn: this.handlerBadBtn,
-  };
-
-  countTotalFeedback = input => {
-    const { good, neutral, bad } = input;
+  const countTotalFeedback = state => {
+    const { good, neutral, bad } = state;
     const total = good + bad + neutral;
     return total;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good, neutral, bad } = this.state;
+  const countPositiveFeedbackPercentage = () => {
+    const { good, neutral, bad } = state;
     const total = good + neutral + bad;
     const percentage = total > 0 ? Math.round((good / total) * 100) : 0;
     return percentage;
   };
 
-  positivePercentage = {
-    totalFeedback: this.countTotalFeedback,
-    positiveFeedbackPercentage: this.countPositiveFeedbackPercentage,
+  const positivePercentage = {
+    totalFeedback: countTotalFeedback,
+    positiveFeedbackPercentage: countPositiveFeedbackPercentage,
   };
 
-  // ================ render area =====================
+  const markupLeaveFeedback = (
+    <Section title="Please leave feedback">
+      <FeedbackOptions onLeaveFeedback={onLeaveFeedback} />
+    </Section>
+  );
 
-  render() {
-    const markupLeaveFrrdback = (
-      <Section title="Please leave feedback">
-        <FeedbackOptions onLeaveFeedback={this.onLeaveFeedback} />
-      </Section>
-    );
+  const markupStatistics = (
+    <Section title="Statistics">
+      <Statistics options={state} positivePercentage={positivePercentage} />
+    </Section>
+  );
 
-    const markupStatistics = (
-      <Section title="Statistics">
-        <Statistics
-          options={this.state}
-          positivePercentage={this.positivePercentage}
-        />
-      </Section>
-    );
+  const message = 'There is no feedback';
 
-    const message = "There is no feedback"
+  return (
+    <div className={css.section}>
+      {markupLeaveFeedback}
+      {good > 0 || neutral > 0 || bad > 0 ? (
+        markupStatistics
+      ) : (
+        <p className={css.message}>{message}</p>
+      )}
+    </div>
+  );
+};
 
-    return (
-      // <div className={css.section}>
-      //   {markupLeaveFrrdback}
-      //   {(this.state.good > 0 ||
-      //     this.state.neutral > 0 ||
-      //     this.state.bad > 0) &&
-      //     markupStatistics}
-      // </div>
-
-      <div className={css.section}>
-        {markupLeaveFrrdback}
-        {(this.state.good > 0 ||
-          this.state.neutral > 0 ||
-          this.state.bad > 0) 
-          ? markupStatistics
-          : <p className={css.message}>{message}</p>
-          }
-      </div>
-
-
-    );
-  }
-}
 export default App;
